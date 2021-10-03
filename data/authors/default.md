2fc16e6244712af9a592d388de9cecc523780296
Ian Duncan <ian@mercury.com>
2021-10-03 10:39:59 +0200

    tweaks

--- data/authors/default.md
+++ data/authors/default.md
@@ -9,8 +9,6 @@ linkedin: https://www.linkedin.com
 github: https://github.com/iand675
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
+I'm Ian Duncan, engineer at Mercury Technologies. Formerly at Forge Global, Betterteam, and CircleCI.
 
-He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+I write about functional programming (especially Haskell), web systems engineering, and PostgreSQL.
--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">
+            Articles
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 font-serif">
             {siteMetadata.description}
           </p>
         </div>
@@ -87,7 +87,7 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="all posts"
           >
-            All Posts &rarr;
+            Older articles available here &rarr;
           </Link>
         </div>
       )}
